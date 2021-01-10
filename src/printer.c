6e44b2d090f6d974f52a644be45a7b8415a79546
Graeme Lockley <graemel@no9.co.za>
2021-01-10 08:57:53 +0000

    feature: support printing of recursive structures

--- src/printer.c
+++ src/printer.c
@@ -12,6 +12,47 @@ typedef struct WriteBufferStruct
     int end_of_writer;
 } WriteBuffer;
 
+struct Set
+{
+    Value *v;
+    struct Set *next;
+};
+
+static int set_in(struct Set *s, Value *v)
+{
+    while (1)
+    {
+        if (s == NULL)
+            return 0;
+
+        if (s->v == v)
+            return 1;
+
+        s = s->next;
+    }
+}
+
+static void set_include(struct Set **s, Value *v)
+{
+    struct Set *result = (struct Set *)malloc(sizeof(struct Set));
+    result->v = v;
+    result->next = *s;
+    *s = result;
+}
+
+static void set_free(struct Set *s)
+{
+    while (1)
+    {
+        if (s == NULL)
+            return;
+
+        struct Set *next = s->next;
+        free(s);
+        s = next;
+    }
+}
+
 static void append(WriteBuffer *wb, char *v)
 {
     int length_of_v = strlen(v);
@@ -30,8 +71,13 @@ static void append(WriteBuffer *wb, char *v)
     wb->end_of_writer += length_of_v;
 }
 
-static void pString(WriteBuffer *wb, Value *v, int readable)
+static void pString(struct Set **s, WriteBuffer *wb, Value *v, int readable)
 {
+    int v_in_set = set_in(*s, v);
+
+    if (!v_in_set)
+        set_include(s, v);
+
     switch (TAG_TO_VT(v))
     {
     case VT_NIL:
@@ -41,9 +87,9 @@ static void pString(WriteBuffer *wb, Value *v, int readable)
     case VT_PAIR:
     {
         if (IS_NIL(v))
-        {
             append(wb, "()");
-        }
+        else if (v_in_set)
+            append(wb, "(...)");
         else
         {
             append(wb, "(");
@@ -52,7 +98,7 @@ static void pString(WriteBuffer *wb, Value *v, int readable)
             {
                 if (IS_PAIR(v))
                 {
-                    pString(wb, CAR(v), readable);
+                    pString(s, wb, CAR(v), readable);
                     v = CDR(v);
                     if (IS_NIL(v))
                     {
@@ -66,7 +112,7 @@ static void pString(WriteBuffer *wb, Value *v, int readable)
                 }
                 else
                 {
-                    pString(wb, v, readable);
+                    pString(s, wb, v, readable);
                     append(wb, ")");
                     break;
                 }
@@ -77,16 +123,23 @@ static void pString(WriteBuffer *wb, Value *v, int readable)
 
     case VT_VECTOR:
     {
-        append(wb, "[");
-        for (int lp = 0; lp < VECTOR(v).length; lp += 1)
+        if (VECTOR(v).length == 0)
+            append(wb, "[]");
+        else if (v_in_set)
+            append(wb, "[...]");
+        else
         {
-            if (lp > 0)
+            append(wb, "[");
+            for (int lp = 0; lp < VECTOR(v).length; lp += 1)
             {
-                append(wb, " ");
+                if (lp > 0)
+                {
+                    append(wb, " ");
+                }
+                pString(s, wb, VECTOR(v).items[lp], readable);
             }
-            pString(wb, VECTOR(v).items[lp], readable);
+            append(wb, "]");
         }
-        append(wb, "]");
         break;
     }
 
@@ -125,26 +178,33 @@ static void pString(WriteBuffer *wb, Value *v, int readable)
 
     case VT_MAP:
     {
-        append(wb, "{");
-
-        Value *cursor = MAP(v);
-        if (!IS_NIL(cursor))
+        if (IS_NIL(MAP(v)))
+            append(wb, "{}");
+        else if (v_in_set)
+            append(wb, "{...}");
+        else
         {
-            while (1)
+            append(wb, "{");
+
+            Value *cursor = MAP(v);
+            if (!IS_NIL(cursor))
             {
-                pString(wb, CAR(CAR(cursor)), readable);
-                append(wb, " ");
-                pString(wb, CDR(CAR(cursor)), readable);
-
-                cursor = CDR(cursor);
-                if (IS_NIL(cursor))
-                    break;
-                else
+                while (1)
+                {
+                    pString(s, wb, CAR(CAR(cursor)), readable);
                     append(wb, " ");
-            }
-        }
+                    pString(s, wb, CDR(CAR(cursor)), readable);
 
-        append(wb, "}");
+                    cursor = CDR(cursor);
+                    if (IS_NIL(cursor))
+                        break;
+                    else
+                        append(wb, " ");
+                }
+            }
+
+            append(wb, "}");
+        }
         break;
     }
 
@@ -167,7 +227,11 @@ Value *Printer_prStr(Value *v, int readable)
     WriteBuffer wb = {malloc(BUFFER_TRANCHE), BUFFER_TRANCHE, 0};
     wb.buffer[0] = (char)0;
 
-    pString(&wb, v, readable);
+    struct Set *values = NULL;
+
+    pString(&values, &wb, v, readable);
+
+    set_free(values);
 
     return mkString(wb.buffer);
 }

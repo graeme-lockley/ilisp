f181184df6e8396f85b3aba9786e6701a0012dd2
Graeme Lockley <graemel@no9.co.za>
2021-02-05 04:26:34 +0000

    fix: remove logging

--- src/builtins.c
+++ src/builtins.c
@@ -925,8 +925,6 @@ Value *builtin_map(Value *parameters, Value *env)
         if (IS_NIL(args) || !IS_PAIR(args))
             return root;
 
-        builtin_prn(mkPair(f, mkPair(CAR(args), VNil)), env);
-
         Value *v = Repl_eval_procedure(f, mkPair(CAR(args), VNil), env);
         if (IS_EXCEPTION(v))
             return v;

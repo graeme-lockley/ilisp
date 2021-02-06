de3ff62968d4b97a2643073d8da4ab6175f5d3d8
Graeme Lockley <graemel@no9.co.za>
2021-02-06 11:07:53 +0000

    feature: load a prelude during startup

--- lib/unit.scm
+++ lib/unit.scm
@@ -1,5 +1,5 @@
 (export-macro (assert-msg-equals msg v1 v2)
-    `(do (define __v1 ~v1) (define __v2 ~v2) (if (= __v1 __v2) () (raise 'AssertionFailed {:msg ~msg :v1 '~v1 :v2 '~v2 :__v1 __v1 :__v2 __v2}))))
+    `(do (define __v1 ~v1) (define __v2 ~v2) (if (= __v1 __v2) () (raise 'AssertionFailed {:msg ~msg :v1 '~v1 :v2 '~v2 :__v1 __v1 :__v2 __v2 :src *source-name*}))))
 
 (export-macro (assert-equals v1 v2)
     `(do 
@@ -7,7 +7,7 @@
         (define __v2 ~v2) 
         (if (= __v1 __v2) 
             () 
-            (raise 'AssertionFailed {:v1 '~v1 :v2 '~v2 :__v1 __v1 :__v2 __v2})
+            (raise 'AssertionFailed {:v1 '~v1 :v2 '~v2 :__v1 __v1 :__v2 __v2 :src *source-name*})
         )
     )
 )
--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
 #include "repl.h"
 #include "value.h"
 
-Value *runtime_env(char *envp[])
+static Value *runtime_env(char *envp[])
 {
     Value *e = map_create();
 
@@ -27,13 +27,85 @@ Value *runtime_env(char *envp[])
     return e;
 }
 
+static char *verify_prelude_file_name(char *prelude_file_name)
+{
+    char *name = realpath(prelude_file_name, NULL);
+
+    if (name == NULL)
+    {
+        printf("File %s does not exists\n", prelude_file_name);
+        exit(-1);
+    }
+
+    return name;
+}
+
+static void run_prelude(char *prelude_file_name, Value *env)
+{
+    if (prelude_file_name == NULL)
+        prelude_file_name = verify_prelude_file_name("prelude.scm");
+
+    char *content = (char *)malloc(strlen(prelude_file_name) + 20);
+    sprintf(content, "(load-file \"%s\")", prelude_file_name);
+
+    printf("Loading %s\n", prelude_file_name);
+
+    Value *result = Repl_rep(prelude_file_name, content, env);
+
+    if (IS_EXCEPTION(result))
+    {
+        printf("Exception executing %s: ", prelude_file_name);
+        builtin_println(result, env);
+        exit(-1);
+    }
+}
+
 int main(int argc, char *argv[], char *envp[])
 {
+    char *prelude_file_name = getenv("ILISP_PRELUDE");
+    if (prelude_file_name != NULL)
+        prelude_file_name = verify_prelude_file_name(prelude_file_name);
+
     Value *env = builtins_initialise_environment();
     map_set_bang(CAR(env), mkSymbol("**env**"), runtime_env(envp));
 
-    if (argc > 1)
+    int arg_idx = 1;
+
+    while (1)
     {
+        if (arg_idx >= argc)
+        {
+            map_set_bang(CAR(env), mkSymbol("*args*"), VNil);
+            run_prelude(prelude_file_name, env);
+            return Repl_repl(env);
+        }
+
+        if (strcmp(argv[arg_idx], "--prelude") == 0)
+        {
+            arg_idx += 1;
+            if (arg_idx < argc)
+            {
+                if (prelude_file_name != NULL)
+                    free(prelude_file_name);
+
+                prelude_file_name = verify_prelude_file_name(argv[arg_idx]);
+                arg_idx += 1;
+            }
+            else
+            {
+                printf("A file name needs to follow the --prelude option\n");
+                exit(-1);
+            }
+            continue;
+        }
+
+        char *script_name = realpath(argv[arg_idx], NULL);
+        if (script_name == NULL)
+        {
+            printf("File %s does not exists\n", argv[1]);
+            exit(-1);
+        }
+
         Value *args = VNil;
         Value **args_cursor = &args;
         for (int lp = 2; lp < argc; lp += 1)
@@ -45,11 +117,7 @@ int main(int argc, char *argv[], char *envp[])
 
         map_set_bang(CAR(env), mkSymbol("*args*"), args);
 
-        char *script_name = realpath(argv[1], NULL);
-        if (script_name == NULL) {
-            printf("File %s does not exists\n", argv[1]);
-            exit(-1);
-        }
+        run_prelude(prelude_file_name, env);
 
         char *content = (char *)malloc(strlen(script_name) + 20);
         sprintf(content, "(load-file \"%s\")", script_name);
@@ -60,10 +128,7 @@ int main(int argc, char *argv[], char *envp[])
         if (IS_EXCEPTION(result))
             printf("Exception: ");
         builtin_println(result, env);
-    }
-    else
-    {
-        map_set_bang(CAR(env), mkSymbol("*args*"), VNil);
-        return Repl_repl(env);
+
+        return IS_EXCEPTION(result) ? -1 : 0;
     }
 }

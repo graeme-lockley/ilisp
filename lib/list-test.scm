ffab6028189dabb125f9de251481cdc988988221
Graeme Lockley <graemel@no9.co.za>
2021-02-07 19:34:27 +0000

    test: place list filter into test

--- lib/list-test.scm
+++ lib/list-test.scm
@@ -4,13 +4,15 @@
 
 (define (even? n) (= (* (/ n 2) 2) n))
 
-(Unit.assert-equals (List.filter '() even?) '())
-(Unit.assert-equals (List.filter '(1 3 5 7 9 11) even?) '())
-(Unit.assert-equals (List.filter '(1 2 3 4 5) even?) '(2 4))
-(Unit.assert-equals (List.filter '(2 4 6 8 10 12) even?) '(2 4 6 8 10 12))
+(Unit.test "filter"
+    (Unit.assert-equals (List.filter '() even?) '())
+    (Unit.assert-equals (List.filter '(1 3 5 7 9 11) even?) '())
+    (Unit.assert-equals (List.filter '(1 2 3 4 5) even?) '(2 4))
+    (Unit.assert-equals (List.filter '(2 4 6 8 10 12) even?) '(2 4 6 8 10 12))
 
-(Unit.assert-signal (List.filter [2 4 6 8 10 12] even?) (fn (signal) (do
-    (Unit.assert-equals (car signal) 'InvalidArgument)
-    (Unit.assert-equals (get (cdr signal) :arg-number) 0)
-    (Unit.assert-equals (get (cdr signal) :procedure) 'list-filter)
-)))
+    (Unit.assert-signal (List.filter [2 4 6 8 10 12] even?) (fn (signal) (do
+        (Unit.assert-equals (car signal) 'InvalidArgument)
+        (Unit.assert-equals (get (cdr signal) :arg-number) 0)
+        (Unit.assert-equals (get (cdr signal) :procedure) 'list-filter)
+    )))
+)

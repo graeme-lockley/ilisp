67500cdf236bf8ef78453c65fd5f4518a414ce4c
Graeme Lockley <graemel@no9.co.za>
2021-03-27 11:00:44 +0000

    test: null?

---
+++ test/builtin-nullp-test.scm
@@ -0,0 +1,27 @@
+(import "../lib/unit.scm" :as Unit)
+
+(Unit.test "null?"
+
+    (Unit.assert-falsy (*builtin*.null? (*builtin*.atom 123)))
+    (Unit.assert-falsy (*builtin*.null? 1))
+    (Unit.assert-truthy (*builtin*.null? ()))
+    (Unit.assert-falsy (*builtin*.null? #t))
+    (Unit.assert-falsy (*builtin*.null? #f))
+    (Unit.assert-falsy (*builtin*.null? #\A))
+    (Unit.assert-falsy (*builtin*.null? "name"))
+    (Unit.assert-falsy (*builtin*.null? 'name))
+    (Unit.assert-falsy (*builtin*.null? :name))
+    (Unit.assert-falsy (*builtin*.null? '(1 2 3)))
+    (Unit.assert-falsy (*builtin*.null? []))
+    (Unit.assert-falsy (*builtin*.null? [1 2 3]))
+    (Unit.assert-falsy (*builtin*.null? {}))
+    (Unit.assert-falsy (*builtin*.null? {:a 1 :b 2 :c 3}))
+    (Unit.assert-falsy (*builtin*.null? (proc (n) (+ n 1))))
+    (Unit.assert-falsy (*builtin*.null? car))
+    (Unit.assert-falsy (*builtin*.null? and))
+)
+
+(Unit.test "arg count signal"
+    (Unit.assert-signal-name (*builtin*.null? ) 'ExpectedArgumentCount)
+    (Unit.assert-signal-name (*builtin*.null? 1 2) 'ExpectedArgumentCount)
+)

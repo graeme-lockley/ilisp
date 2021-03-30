a31b854b77ec89eff70e002eaae9cb8412e1f49f
Graeme Lockley <graeme.lockley@SBDBNN95QVNQ2.INVESTEC.CORP>
2021-03-30 11:34:21 +0200

    refactor: break builtin_apply_wrapped into two

--- src/builtin/apply.c
+++ src/builtin/apply.c
@@ -5,20 +5,13 @@
 #include "../repl.h"
 #include "../value.h"
 
-Value *builtin_apply_wrapped(Value *parameters, Value *env)
+Value *builtin_apply(Value *f, Value *rest, Value *env)
 {
-    if (!IS_PAIR(parameters))
-        return exceptions_invalid_argument(mkSymbol("apply"), 0, mkSymbol("pair"), parameters);
-
-    Value *f = CAR(parameters);
-
     if (!IS_PROCEDURE(f) && !IS_NATIVE_PROCEDURE(f) && !IS_KEYWORD(f))
-        return exceptions_invalid_argument(mkSymbol("apply"), 0, mkSymbol("procedure"), f);
+        return exceptions_invalid_argument(mkSymbol("*builtin*.apply"), 0, mkSymbol("procedure"), f);
 
-    parameters = CDR(parameters);
-
-    if (!IS_PAIR(parameters))
-        return exceptions_invalid_argument(mkSymbol("apply"), 1, mkSymbol("pair"), parameters);
+    if (!IS_PAIR(rest))
+        return exceptions_invalid_argument(mkSymbol("*builtin*.apply"), 1, mkSymbol("pair"), rest);
 
     Value *root = VNull;
     Value **root_cursor = &root;
@@ -26,36 +19,46 @@ Value *builtin_apply_wrapped(Value *parameters, Value *env)
 
     while (1)
     {
-        if (IS_NULL(CDR(parameters)))
+        if (IS_NULL(CDR(rest)))
         {
-            parameters = CAR(parameters);
+            rest = CAR(rest);
 
-            if (IS_VECTOR(parameters))
-                parameters = builtin_vector_to_list(parameters);
+            if (IS_VECTOR(rest))
+                rest = builtin_vector_to_list(rest);
 
-            if (!IS_PAIR(parameters) && !IS_NULL(parameters))
-                return exceptions_invalid_argument(mkSymbol("apply"), parameter_count, mkSymbol("pair"), parameters);
+            if (!IS_PAIR(rest) && !IS_NULL(rest))
+                return exceptions_invalid_argument(mkSymbol("apply"), parameter_count, mkSymbol("pair"), rest);
 
             while (1)
             {
-                if (IS_NULL(parameters))
+                if (IS_NULL(rest))
                     return Repl_eval_procedure(f, root, env);
 
-                Value *v = mkPair(CAR(parameters), VNull);
+                Value *v = mkPair(CAR(rest), VNull);
                 *root_cursor = v;
                 root_cursor = &CDR(v);
 
-                parameters = CDR(parameters);
+                rest = CDR(rest);
             }
         }
 
-        Value *v = mkPair(CAR(parameters), VNull);
+        Value *v = mkPair(CAR(rest), VNull);
         *root_cursor = v;
         root_cursor = &CDR(v);
 
-        parameters = CDR(parameters);
+        rest = CDR(rest);
         parameter_count += 1;
     }
 
     return VNull;
+}
+
+Value *builtin_apply_wrapped(Value *parameters, Value *env)
+{
+    if (!IS_PAIR(parameters))
+        return exceptions_invalid_argument(mkSymbol("*builtin*.apply"), 0, mkSymbol("pair"), parameters);
+
+    Value *f = CAR(parameters);
+    Value *rest = CDR(parameters);
+    return builtin_apply(f, rest, env);
 }

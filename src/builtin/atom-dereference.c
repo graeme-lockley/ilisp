6ae21b7fbc3087602fcc09f46f57ae2308cb27c2
Graeme Lockley <graeme.lockley@SBDBNN95QVNQ2.INVESTEC.CORP>
2021-03-30 17:40:20 +0200

    refactor: split builtin_atom_dereference_wrapped

--- src/builtin/atom-dereference.c
+++ src/builtin/atom-dereference.c
@@ -4,9 +4,12 @@
 #include "../exceptions.h"
 #include "../value.h"
 
-Value *builtin_atom_dereference(Value *value)
+Value *builtin_atom_dereference(Value *atom)
 {
-    return ATOM(value);
+    if (!IS_ATOM(atom))
+        return exceptions_invalid_argument(mkSymbol("*builtin*.atom-dereference"), 0, mkSymbol("atom"), atom);
+
+    return ATOM(atom);
 }
 
 Value *builtin_atom_dereference_wrapped(Value *parameters, Value *env)
@@ -16,9 +19,6 @@ Value *builtin_atom_dereference_wrapped(Value *parameters, Value *env)
     Value *extract_result = extract_fixed_parameters(parameter, parameters, 1, "*builtin*.atom-dereference");
     if (extract_result != NULL)
         return extract_result;
-
-    if (!IS_ATOM(parameter[0]))
-        return exceptions_invalid_argument(mkSymbol("*builtin*.atom-dereference"), 0, mkSymbol("atom"), parameter[0]);
 
     return builtin_atom_dereference(parameter[0]);
 }

bcc462eb89be136ef9ff17ae7093045706412201
Graeme Lockley <graemel@no9.co.za>
2021-06-05 06:03:35 +0000

    feature: operand get element ptr

--- lib/mini-ilisp/llvm/ir/operand-test.scm
+++ lib/mini-ilisp/llvm/ir/operand-test.scm
@@ -16,3 +16,17 @@
     (Unit.assert-equals (Operand.untyped-operand->string operand) "127")
     (Unit.assert-equals (Operand.typed-operand->string operand) "i8 127")
 )
+
+(Unit.test "operand->string get element ptr"
+    (const operand (Operand.GetElementPtr 
+        #t 
+        (Type.Pointer Type.i8)
+        (Type.Array 12 Type.i8)
+        (Operand.LocalReference "@.str" (Type.Pointer (Type.Array 12 Type.i8)))
+        (list (Operand.CInt 64 0) (Operand.CInt 64 0))
+    ))
+
+    (Unit.assert-equals (Operand.untyped-operand->string operand) "getelementptr inbounds ([12 x i8], [12 x i8]* @.str, i64 0, i64 0)")
+    (Unit.assert-equals (Operand.typed-operand->string operand) "i8* getelementptr inbounds ([12 x i8], [12 x i8]* @.str, i64 0, i64 0)")
+)
+
--- lib/mini-ilisp/llvm/ir/operand.scm
+++ lib/mini-ilisp/llvm/ir/operand.scm
@@ -11,19 +11,53 @@
     (type Type?)
 )
 
+(struct GetElementPtr
+    (inbounds boolean?)
+    (type Type?)
+    (element-type Type?)
+    (address Operand?)
+    (indices (list-of? Constant?))
+)
+
 (struct CInt
     (bits number?)
     (value number?)
 )
 
+(union Operand
+    LocalReference? GetElementPtr? Constant?
+)
+
+(union Constant
+    CInt?
+)
+
 (const (untyped-operand->string operand)
     (if (LocalReference? operand) (LocalReference-name operand)
         (CInt? operand) (str (CInt-value operand))
+        (GetElementPtr? operand) (get-element-ptr->string operand #f)
     )
 )
 
 (const (typed-operand->string operand)
     (if (LocalReference? operand) (str (type->string (LocalReference-type operand)) " " (LocalReference-name operand))
         (CInt? operand) (str "i" (CInt-bits operand) " " (CInt-value operand))
+        (GetElementPtr? operand) (get-element-ptr->string operand #t)
+    )
+)
+
+(const- (get-element-ptr->string operand show-type)
+    (str
+        (if show-type (str (type->string (GetElementPtr-type operand)) " ") "")
+        "getelementptr"
+        (if (GetElementPtr-inbounds operand) " inbounds " " ")
+        "("
+        (type->string (GetElementPtr-element-type operand))
+        ", "
+        (typed-operand->string (GetElementPtr-address operand))
+        (*builtin*.apply str
+            (List.map (GetElementPtr-indices operand) (proc (i) (str ", " (typed-operand->string i))))
+        )
+        ")"
     )
 )

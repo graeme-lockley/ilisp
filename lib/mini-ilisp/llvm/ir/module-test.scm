bd734b3c9ef84bcf5f4535ca0829e5aec03d38a2
Graeme Lockley <graemel@no9.co.za>
2021-06-06 07:02:55 +0000

    feature: global->string

--- lib/mini-ilisp/llvm/ir/module-test.scm
+++ lib/mini-ilisp/llvm/ir/module-test.scm
@@ -1,7 +1,8 @@
 (import "../../../unit.scm" :as Unit)
 
-(import "./type.scm" :as Type)
 (import "./module.scm" :as Module)
+(import "./operand.scm" :as Operand)
+(import "./type.scm" :as Type)
 
 (Unit.test "identified-type->string"
     (Unit.assert-equals 
@@ -29,5 +30,32 @@
     (Unit.assert-equals 
         (Module.external->string (Module.External "@printf" Type.i32 (list (Type.Pointer Type.i8)) #t))
         "declare i32 @printf(i8*, ...)"
+    )
+)
+
+(Unit.test "global->string"
+    (Unit.assert-equals 
+        (Module.global->string (Module.Global "@.str0" (Type.Array 12 Type.i8) (Operand.string->carray "hello world") #t #t #t 0))
+        "@.str0 = private unnamed_addr constant [12 x i8] [i8 104, i8 101, i8 108, i8 108, i8 111, i8 32, i8 119, i8 111, i8 114, i8 108, i8 100, i8 0]"
+    )
+
+    (Unit.assert-equals 
+        (Module.global->string (Module.Global "@.str0" (Type.Array 12 Type.i8) (Operand.string->carray "hello world") #t #t #t 1))
+        "@.str0 = private unnamed_addr constant [12 x i8] [i8 104, i8 101, i8 108, i8 108, i8 111, i8 32, i8 119, i8 111, i8 114, i8 108, i8 100, i8 0], align 1"
+    )
+
+    (Unit.assert-equals 
+        (Module.global->string (Module.Global "@.str0" (Type.Array 12 Type.i8) (Operand.string->carray "hello world") #f #t #t 1))
+        "@.str0 = unnamed_addr constant [12 x i8] [i8 104, i8 101, i8 108, i8 108, i8 111, i8 32, i8 119, i8 111, i8 114, i8 108, i8 100, i8 0], align 1"
+    )
+
+    (Unit.assert-equals 
+        (Module.global->string (Module.Global "@.str0" (Type.Array 12 Type.i8) (Operand.string->carray "hello world") #t #f #t 1))
+        "@.str0 = private constant [12 x i8] [i8 104, i8 101, i8 108, i8 108, i8 111, i8 32, i8 119, i8 111, i8 114, i8 108, i8 100, i8 0], align 1"
+    )
+
+    (Unit.assert-equals 
+        (Module.global->string (Module.Global "@.str0" (Type.Array 12 Type.i8) (Operand.string->carray "hello world") #t #t #f 1))
+        "@.str0 = private unnamed_addr [12 x i8] [i8 104, i8 101, i8 108, i8 108, i8 111, i8 32, i8 119, i8 111, i8 114, i8 108, i8 100, i8 0], align 1"
     )
 )
--- lib/mini-ilisp/llvm/ir/module.scm
+++ lib/mini-ilisp/llvm/ir/module.scm
@@ -3,6 +3,7 @@
 (import "../../../predicate.scm" :names =? boolean? byte-vector? character? integer? list? list-of? null? string?)
 (import "../../../string.scm" :as String)
 (import "./type.scm" :names Type? type->string)
+(import "./operand.scm" :as Operand)
 
 (struct IdentifiedType
     (name string?)
@@ -14,6 +15,17 @@
     (return-type Type?)
     (parameter-types (list-of? Type?))
     (is-var-arg boolean?)
+)
+
+(struct Global
+    (name string?)
+    (type Type?)
+    (value Operand.Constant?)
+
+    (private boolean?)
+    (unnamed-addr boolean?)
+    (constant boolean?)
+    (align number?)
 )
 
 (const (identified-type->string identified-type)
@@ -35,4 +47,18 @@
     )
 
     (str "declare " (type->string (External-return-type external)) " " (External-name external) "(" parameters ")")
+)
+
+(const (global->string global)
+    (str
+        (Global-name global)
+        " = "
+        (if (Global-private global) "private " "")
+        (if (Global-unnamed-addr global) "unnamed_addr " "")
+        (if (Global-constant global) "constant " "")
+        (type->string (Global-type global))
+        " "
+        (Operand.untyped-operand->string (Global-value global))
+        (if (= 0 (Global-align global)) "" (str ", align " (Global-align global)))
+    )
 )

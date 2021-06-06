522167c4654bd9929e6da9f3c39a5419d05b263e
Graeme Lockley <graemel@no9.co.za>
2021-06-06 08:33:45 +0000

    feature: instruction call

--- lib/mini-ilisp/llvm/ir/instruction-test.scm
+++ lib/mini-ilisp/llvm/ir/instruction-test.scm
@@ -6,6 +6,18 @@
 (import "./operand.scm" :as Operand)
 (import "./type.scm" :as Type)
 
+(Unit.test "instruction->string Call"
+    (Unit.assert-equals 
+        (Instruction.instruction->string (Instruction.Call (Operand.LocalReference "%1" Type.i32) "@_fred" ()))
+        "  %1 = call i32 @_fred()\n"
+    )
+
+    (Unit.assert-equals 
+        (Instruction.instruction->string (Instruction.Call (Operand.LocalReference "%2" Type.i32) "@_fred" (list (Operand.LocalReference "%1" (Type.Pointer (Type.Reference "%struct.Value"))))))
+        "  %2 = call i32 @_fred(%struct.Value* %1)\n"
+    )
+)
+
 (Unit.test "instruction->string CallVoid"
     (Unit.assert-equals 
         (Instruction.instruction->string (Instruction.CallVoid "@_print_newline" ()))
--- lib/mini-ilisp/llvm/ir/instruction.scm
+++ lib/mini-ilisp/llvm/ir/instruction.scm
@@ -4,15 +4,34 @@
 (import "../../../list.scm" :as List)
 (import "../../../string.scm" :as String)
 
-(import "./operand.scm" :names Operand?)
+(import "./operand.scm" :as Operand)
+(import "./type.scm" :as Type)
+
+(struct Call
+    (return Operand.LocalReference?)
+    (name string?)
+    (arguments (list-of? Operand.Operand?))
+)
 
 (struct CallVoid
     (name string?)
-    (arguments (list-of? Operand?))
+    (arguments (list-of? Operand.Operand?))
 )
 
 (const (instruction->string instruction)
-    (if (CallVoid? instruction)
+    (if (Call? instruction)
+            (str 
+                "  "
+                (Operand.LocalReference-name (Call-return instruction))
+                " = call "
+                (Type.type->string (Operand.LocalReference-type (Call-return instruction)))
+                " "
+                (Call-name instruction)
+                "("
+                (String.interpolate-with (List.map (Call-arguments instruction) Operand.typed-operand->string) ", ")
+                ")\n"
+            )
+        (CallVoid? instruction)
             (str 
                 "  call void "
                 (CallVoid-name instruction)

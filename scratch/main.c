8ae7d176501652c2dbdacccaabfea55f508db8b5
Graeme Lockley <graemel@no9.co.za>
2021-06-10 06:11:35 +0000

    feature: show printing boolean values

--- scratch/main.c
+++ scratch/main.c
@@ -5,6 +5,12 @@
 
 int main()
 {
-	_print_value(_from_literal_string("hello world"));
+	_initialise_lib();
+	_print_value(_from_literal_string("hello world, "));
+	_print_value(_from_literal_int(123));
+	_print_value(_from_literal_string(", "));
+	_print_value(VTrue);
+	_print_value(_from_literal_string(", "));
+	_print_value(VFalse);
 	_print_newline();
 }

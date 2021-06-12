45ab83f34bf94318fc034925fd87dc67794d7781
Graeme Lockley <graemel@no9.co.za>
2021-06-12 07:08:32 +0000

    refactor: move to top-level procedures

--- lib/mini-ilisp/static.scm
+++ lib/mini-ilisp/static.scm
@@ -13,114 +13,6 @@
 ; - Change the S-Expression representation into a representation that is more
 ;   amenable to translation into LLVM IR.
 (const (ast->tst ast)
-    (const (plus->tst env e)
-        (const es (AST.S-Expression-expressions e))
-        (const es' (List.map (cdr es) (proc (e') (expression->tst env e'))))
-
-        (if (null? es') (TST.IntegerLiteral 0)
-            (= (count es') 1) (car es')
-            (fold (cddr es') (TST.Plus (nth es' 0) (nth es' 1)) TST.Plus)
-        )
-    )
-
-    (const (minus->tst env e)
-        (const es (AST.S-Expression-expressions e))
-        (const es' (List.map (cdr es) (proc (e') (expression->tst env e'))))
-
-        (if (null? es') (TST.IntegerLiteral 0)
-            (= (count es') 1) (TST.Minus (TST.IntegerLiteral 0) (car es'))
-            (fold (cddr es') (TST.Minus (nth es' 0) (nth es' 1)) TST.Minus)
-        )
-    )
-
-    (const (multiply->tst env e)
-        (const es (AST.S-Expression-expressions e))
-        (const es' (List.map (cdr es) (proc (e') (expression->tst env e'))))
-
-        (if (null? es') (TST.IntegerLiteral 1)
-            (= (count es') 1) (car es')
-            (fold (cddr es') (TST.Multiply (nth es' 0) (nth es' 1)) TST.Multiply)
-        )
-    )
-
-    (const (divide->tst env e)
-        (const es (AST.S-Expression-expressions e))
-        (const es' (List.map (cdr es) (proc (e') (expression->tst env e'))))
-
-        (if (null? es') (TST.IntegerLiteral 1)
-            (= (count es') 1) (TST.Divide (TST.IntegerLiteral 1) (car es'))
-            (fold (cddr es') (TST.Divide (nth es' 0) (nth es' 1)) TST.Divide)
-        )
-    )
-
-    (const (print-expression->tst env e)
-        (const es (AST.S-Expression-expressions e))
-        (const es' (List.map (cdr es) (proc (e') (expression->tst env e'))))
-
-        (TST.CallPrint es')
-    )
-
-    (const (println-expression->tst env e)
-        (const es (AST.S-Expression-expressions e))
-        (const es' (List.map (cdr es) (proc (e') (expression->tst env e'))))
-
-        (TST.CallPrintLn es')
-    )
-
-    ;; ; A const binding has 2 forms - single name and list of names.
-    ;; (const (const-expression->tst env e)
-    ;;     (const es (AST.S-Expression-expressions e))
-
-    ;;     (if (< (count es) 3) (raise 'InvalidForm {:form 'const :syntax es :location (const expressions (AST.S-Expression-location e))}))
-
-    ;;     (const args (cadr es))
-    ;;     (const body (cddr es))
-
-    ;;     ;; grab name from args
-    ;;     ;; - error if name is already declared
-    ;;     ;; - place declaration name into current scope
-    ;;     ;; - open scope
-    ;;     ;; - place parameter bindings into new scope
-    ;;     ;;   - report error if duplicate parameter binding
-    ;;     ;; - translate body within the context of the new scope
-    ;;     ;; - close scope
-    ;; )
-
-    (const (expression->tst env e)
-        (if (AST.IdentifierExpression? e)
-                (if (Env.binding? env (AST.IdentifierExpression-id e))
-                        (TST.IdentifierReference (AST.IdentifierExpression-id e) (AST.IdentifierExpression-location e))
-                    (raise 'UnknownIdentifier {:identifier (AST.IdentifierExpression-id e) :location (AST.IdentifierExpression-location e) })
-                )
-            (AST.LiteralIntExpression? e)
-                (integer-literal-expression->tst env e)
-            (AST.LiteralStringExpression? e)
-                (string-literal-expression->tst env e)
-            (AST.LiteralBooleanExpression? e)
-                (TST.BooleanLiteral (AST.LiteralBooleanExpression-value e))
-            (AST.S-Expression? e)
-                (do (const expressions (AST.S-Expression-expressions e))
-                    (const first-expression (car expressions))
-
-                    (if (AST.IdentifierExpression? first-expression)
-                        (do (const first-expression-identifier (AST.IdentifierExpression-id first-expression))
-                            (if (= first-expression-identifier "println") (println-expression->tst env e)
-                                (= first-expression-identifier "print") (print-expression->tst env e)
-                                (= first-expression-identifier "+") (plus->tst env e)
-                                (= first-expression-identifier "-") (minus->tst env e)
-                                (= first-expression-identifier "*") (multiply->tst env e)
-                                (= first-expression-identifier "/") (divide->tst env e)
-                                ;; (= first-expression-identifier "const") (const-expression->tst env e)
-                                (raise 'TODO-1 e)
-                            )
-                        )
-                        (raise 'TODO-2 e)
-                    )
-                )
-            (raise 'TODO-3 e)
-        )
-    )
-
     (const env (Env.empty))
 
     (for-each '(
@@ -136,14 +28,108 @@
     (List.map ast (proc (e) (expression->tst env e)))
 )
 
-(const (integer-literal-expression->tst env e)
+(const- (expression->tst env e)
+    (if (AST.IdentifierExpression? e)
+            (if (Env.binding? env (AST.IdentifierExpression-id e))
+                    (TST.IdentifierReference (AST.IdentifierExpression-id e) (AST.IdentifierExpression-location e))
+                (raise 'UnknownIdentifier {:identifier (AST.IdentifierExpression-id e) :location (AST.IdentifierExpression-location e) })
+            )
+        (AST.LiteralIntExpression? e)
+            (integer-literal-expression->tst env e)
+        (AST.LiteralStringExpression? e)
+            (string-literal-expression->tst env e)
+        (AST.LiteralBooleanExpression? e)
+            (TST.BooleanLiteral (AST.LiteralBooleanExpression-value e))
+        (AST.S-Expression? e)
+            (do (const expressions (AST.S-Expression-expressions e))
+                (const first-expression (car expressions))
+
+                (if (AST.IdentifierExpression? first-expression)
+                    (do (const first-expression-identifier (AST.IdentifierExpression-id first-expression))
+                        (if (= first-expression-identifier "println") (println-expression->tst env e)
+                            (= first-expression-identifier "print") (print-expression->tst env e)
+                            (= first-expression-identifier "+") (plus->tst env e)
+                            (= first-expression-identifier "-") (minus->tst env e)
+                            (= first-expression-identifier "*") (multiply->tst env e)
+                            (= first-expression-identifier "/") (divide->tst env e)
+                            ;; (= first-expression-identifier "const") (const-expression->tst env e)
+                            (raise 'TODO-1 e)
+                        )
+                    )
+                    (raise 'TODO-2 e)
+                )
+            )
+        (raise 'TODO-3 e)
+    )
+)
+
+;; ; A const binding has 2 forms - single name and list of names.
+;; (const- (const-expression->tst env e)
+;;     (const es (AST.S-Expression-expressions e))
+;;
+;;     (if (< (count es) 3) (raise 'InvalidForm {:form 'const :syntax es :location (const expressions (AST.S-Expression-location e))}))
+;;
+;;     (const args (cadr es))
+;;     (const body (cddr es))
+;;
+;;     ;; grab name from args
+;;     ;; - error if name is already declared
+;;     ;; - place declaration name into current scope
+;;     ;; - open scope
+;;     ;; - place parameter bindings into new scope
+;;     ;;   - report error if duplicate parameter binding
+;;     ;; - translate body within the context of the new scope
+;;     ;; - close scope
+;; )
+
+(const- (plus->tst env e)
+    (arithmetic-op->tst env e (TST.IntegerLiteral 0) TST.Plus)
+)
+
+(const- (minus->tst env e)
+    (arithmetic-op->tst env e (TST.IntegerLiteral 0) TST.Minus)
+)
+
+(const- (multiply->tst env e)
+    (arithmetic-op->tst env e (TST.IntegerLiteral 1) TST.Multiply)
+)
+
+(const- (divide->tst env e)
+    (arithmetic-op->tst env e (TST.IntegerLiteral 1) TST.Divide)
+)
+
+(const- (arithmetic-op->tst env e unary op)
+    (const es (AST.S-Expression-expressions e))
+    (const es' (List.map (cdr es) (proc (e') (expression->tst env e'))))
+
+    (if (null? es') unary
+        (= (count es') 1) (op unary (car es'))
+        (fold (cddr es') (op (nth es' 0) (nth es' 1)) op)
+    )
+)
+
+(const- (print-expression->tst env e)
+    (const es (AST.S-Expression-expressions e))
+    (const es' (List.map (cdr es) (proc (e') (expression->tst env e'))))
+
+    (TST.CallPrint es')
+)
+
+(const- (println-expression->tst env e)
+    (const es (AST.S-Expression-expressions e))
+    (const es' (List.map (cdr es) (proc (e') (expression->tst env e'))))
+
+    (TST.CallPrintLn es')
+)
+
+(const- (integer-literal-expression->tst env e)
     (const literal (AST.LiteralIntExpression-value e))
     (const literal-value (Number.string->number literal))
     
     (TST.IntegerLiteral literal-value)
 )
 
-(const (string-literal-expression->tst env e)
+(const- (string-literal-expression->tst env e)
     (const literal (AST.LiteralStringExpression-value e))
     (const literal-value (String.slice literal 1 (- (count literal) 1)))
     
